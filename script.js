fff6c368f7e1d1a0b4f8435b2404153efb15c815
elsergio130v <59649728+elsergio130v@users.noreply.github.com>
2023-10-27 17:34:39 +0200

    objeto bola
    prueba para subir contenido a la rama.

---
+++ script.js
@@ -0,0 +1,14 @@
+const bolaElement = document.querySelector("#bola");
+let bola = { 
+    x:0, y:0,
+    direccion:false, //si va a la izq es false si no, true.
+    color:"white",
+    size:"5px",
+    maxAltura:"600px",
+    minAltura:"0",
+    maxAnho:"1000px",
+    minAncho:"0px",
+};
+
+
+
