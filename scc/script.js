c561e05f2bc990c40e2b7391c707297984a86071
elsergio130v <59649728+elsergio130v@users.noreply.github.com>
2023-10-31 17:36:59 +0100

    cretion movement for the ball to the project

--- scc/script.js
+++ scc/script.js
@@ -1,5 +1,8 @@
 const bolaElement = document.querySelector("#bola");
 const myCanvas = document.querySelector("#myCanvas");
+
+const WIDTH = 800;
+const HEIGHT = 500;
 let bola = { 
     x:0, y:0,
     direccion:false, //si va a la izq es false si no, true.
@@ -9,27 +12,49 @@ let bola = {
     minAltura:"0",
     maxAnho:"1000px",
     minAncho:"0px",
+    posicionBolaEjeX :400,
+    posicionBolaEjeY : 250,
+    velocidadX : 5,
+    velocidadY : 5 
 };
 
-
-
-myCanvas.width = 800;
-myCanvas.height = 500;
-
+myCanvas.height = HEIGHT;
+myCanvas.width = WIDTH;
 let context = myCanvas.getContext("2d");
+pintarRectanguloCanvas();
+moverBola();
+
+
+
+function moverBola() {
+    
+    context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    context.beginPath();
+    context.arc(bola.posicionBolaEjeX, bola.posicionBolaEjeY, 10, 0, 2 * Math.PI);
+    context.fillStyle = "white";
+    context.fill();
+    
+    bola.posicionBolaEjeX += bola.velocidadX;
+    bola.posicionBolaEjeY += bola.velocidadY;
+    
+    requestAnimationFrame(moverBola);
+}
+
+
+function pintarRectanguloCanvas(){
+    context.fillStyle = "black";
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
 
 //Posicionamiento del pincel
-context.beginPath();
 
-context.stroke();
+//context.stroke();
 
-context.closePath();
 
 //Para dibujar un rectángulo
 
-context.fillStyle = "black";
 
-context.fillRect(0, 0, myCanvas.width, myCanvas.height);
 
 
 
